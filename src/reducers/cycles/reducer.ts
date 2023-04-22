c1db23635096902cdd3d071a4a0b44876280e6be
Flávio Santos <flavio.santos@pravaler.com.br>
2023-04-22 14:28:43 -0300

    Ultilizando Immer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -18,59 +18,38 @@ interface CycleState {
 export function CyclesReducers(state: CycleState, action: any) {
   switch (action.type) {
     case ActionTypes.CREATED_NEW_CYCLE_CONTEXT:
-      // return {
-      //   ...state,
-      //   cycles: [...state.cycles, action.payload.newCycle],
-      //   activeCycleId: action.payload.newCycle.id,
-      // }
-
       return produce(state, (draft) => {
         draft.cycles.push(action.payload.newCycle)
         draft.activeCycleId = action.payload.newCycle.id
       })
 
-    case ActionTypes.INTERRUPTED_CYCLES_CONTEXT:
-      {
-        // return {
-        //   ...state,
-        //   cycles: state.cycles.map((cycle) => {
-        //     if (cycle.id === state.activeCycleId) {
-        //       return { ...cycle, interrupDate: new Date() }
-        //     } else {
-        //       return cycle
-        //     }
-        //   }),
-        //   activeCycleId: null,
-        // }
-
+    case ActionTypes.INTERRUPTED_CYCLES_CONTEXT: {
+      return produce(state, (draft) => {
         const currentCycleIndex = state.cycles.findIndex((cycle) => {
           return cycle.id === state.activeCycleId
         })
 
-        console.log('currentCycleIndex', state.cycles[currentCycleIndex])
+        if (currentCycleIndex < 0) {
+          return state
+        }
+
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].interrupDate = new Date()
+      })
+    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      return produce(state, (draft) => {
+        const currentCycleIndex = state.cycles.findIndex((cycle) => {
+          return cycle.id === state.activeCycleId
+        })
 
         if (currentCycleIndex < 0) {
           return state
         }
-      }
-      return produce(state, (draft) => {
         draft.activeCycleId = null
-        console.log('aqui')
-        console.log('draft', draft.cycles[currentCycleIndex])
-        // draft.cycles[currentCycleIndex].interrupDate = new Date()
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
       })
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+    }
     default:
       return state
   }
